bdb6fe38d31c94c4e27461d2834bf3a0249b8c51
Gustavo erazo <erazo.gustavo@gmail.com>
2021-07-30 21:48:42 -0500

    Corrección en conexion

--- config/conexion.php
+++ config/conexion.php
@@ -5,7 +5,7 @@ require_once '../php-activerecord/ActiveRecord.php';
 ActiveRecord\Config::initialize(function($cfg)
 {
     $cfg->set_model_directory('../models/');
-    $cfg->set_connections(array('development' => 'pgsql://postgres:13264984@127.0.0.1/censo_dev2'));
+    $cfg->set_connections(array('development' => 'pgsql://postgres:13264984@127.0.0.1/censo_dev'));
     //$cfg->set_connections(array('development' => 'pgsql://postgres:postgres@127.0.0.1/censo_dev2'));
  	//$cfg->set_connections(array('development' => 'pgsql://gerazo:13264983@130.11.151.202/censo_dev'));
 	// you can change the default connection with the below
