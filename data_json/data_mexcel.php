472ac627c4012aa98a18d504d0540120d0071b77
Juanpas222 <60269419+Juanpas222@users.noreply.github.com>
2021-08-08 09:18:14 -0500

    commit 08082021 revisar

---
+++ data_json/data_mexcel.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../models/Madjuntaexcel.php';
+
+$unid=$_GET["este "];
+	$result = array();
+	//echo($unid);exit();	
+	$data = Madjuntaexcel::find_by_sql("select id,adjunto,no_soli from madjuntaexcel WHERE  no_soli = ".$unid." order by id desc;");
+	$items = 0;
+
+	foreach ($data as &$rs) {
+
+		
+		array_push($result,array(			
+											 "id"=>$rs->id,
+											 "adjunto"=>$rs->adjunto
+		                         ));
+
+		$items++;
+
+	}
+
+//echo($data);exit();
+	print_r(json_encode($result));
+?>
