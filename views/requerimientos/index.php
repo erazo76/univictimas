368d1c197e73b57e1c4fabd413bfcbd29b42e82b
Yonny Ramirez <yonnygr@gmail.com>
2021-10-27 20:52:38 -0400

    auditor

--- views/requerimientos/index.php
+++ views/requerimientos/index.php
@@ -52,14 +52,14 @@ ValidaSession("../login");
 
 		var desh=<?php echo $_SESSION['rolx'];  ?>;//verifica el rol del usuario
 		
-		// if (desh==6){ //si el rol del usuario es AUDITOR...
-		// 	document.getElementById('delete').style.display = 'none';	
-	    //     document.getElementById('add').style.display = 'none';	
+		if (desh==6){ //si el rol del usuario es AUDITOR...
+			document.getElementById('delete').style.display = 'none';	
+	        document.getElementById('add').style.display = 'none';	
 
 
-		// }else{
+		}else{
 
-		// }
+		}
 		//deshabilitar edicion mientras se programa el modulo editar
 		document.getElementById("see").disabled=false;
 
